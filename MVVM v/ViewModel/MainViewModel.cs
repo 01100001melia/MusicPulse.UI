62e8745822f8438fabe0ad9fe0a9ea8c8bb22b5e
Amelia <157375868+01100001melia@users.noreply.github.com>
2025-04-18 01:00:11 +0300

    MainViewModel.cs

--- MVVM v/ViewModel/MainViewModel.cs
+++ MVVM v/ViewModel/MainViewModel.cs
@@ -1,1 +1,33 @@
+using System;
+using MusicPulse.Core;
 
+namespace MusicPulse.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+
+        public HomeViewModel HomeVm{ get; set; }
+
+        private object _CurrentView;
+
+        public object CurrentView
+        {
+            get { return _CurrentView; }
+            set 
+            { _CurrentView = value;
+                OnPropertyChanged();
+            
+            
+            }
+        }
+
+        public  MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
+        }
+
+           
+    }
+}
