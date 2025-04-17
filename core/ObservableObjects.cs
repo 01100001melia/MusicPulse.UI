acdf33f57faaf330acb9049baae34fe938b02f40
Amelia <157375868+01100001melia@users.noreply.github.com>
2025-04-18 00:31:01 +0300

    Update ObservableObjects.cs

--- core/ObservableObjects.cs
+++ core/ObservableObjects.cs
@@ -1,1 +1,16 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MusicPulse.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
 
