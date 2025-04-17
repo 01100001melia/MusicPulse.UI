446675a21d6a2c0ed9e8311c5a52124d629d08f1
Amelia <157375868+01100001melia@users.noreply.github.com>
2025-04-18 01:12:12 +0300

    MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,1 +1,20 @@
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MusicPulse
+{
+   
+    
+   public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
 
